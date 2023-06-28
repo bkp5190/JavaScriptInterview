6a32a2c01b71ea5dd07c6c305d780a990c1bccc0
Boney Patel <boneypatel37@yahoo.com>
2023-06-27 22:34:11 -0400

    Breadth First Search Recursive

---
+++ Algorithms/breadthFirstSearchRecursive.js
@@ -0,0 +1,88 @@
+// Write a Recursive implementation of BFS in JavaScript.
+
+class Node {
+  constructor(value) {
+    this.left = null;
+    this.right = null;
+    this.value = value;
+  }
+}
+
+class BinarySearchTree {
+  constructor() {
+    this.root = null;
+  }
+  insert(value) {
+    const newNode = new Node(value);
+    if (this.root === null) {
+      this.root = newNode;
+    } else {
+      let currentNode = this.root;
+      while (true) {
+        if (value < currentNode.value) {
+          // Left
+          if (!currentNode.left) {
+            currentNode.left = newNode;
+            return this;
+          }
+          currentNode = currentNode.left;
+        } else {
+          // Right
+          if (!currentNode.right) {
+            currentNode.right = newNode;
+            return this;
+          }
+          currentNode = currentNode.right;
+        }
+      }
+    }
+  }
+  lookup(value) {
+    if (!this.root) {
+      return false;
+    }
+    let currentNode = this.root;
+    while (currentNode) {
+      if (value < currentNode.value) {
+        currentNode = currentNode.left;
+      } else if (value > currentNode.value) {
+        currentNode = currentNode.right;
+      } else if (value === currentNode.value) {
+        return currentNode;
+      }
+    }
+    return false;
+  }
+  breadthFirstSearchR(queue, list) {
+    if (!queue.length) {
+        return list;
+    }
+
+    let currentNode = queue.shift();
+    list.push(currentNode.value);
+    if (currentNode.left) {
+        queue.push(currentNode.left);
+    }
+    if (currentNode.right) {
+        queue.push(currentNode.right);
+    }    
+    return this.breadthFirstSearchR(queue, list);
+  }
+}
+
+tree = new BinarySearchTree();
+tree.insert(9);
+tree.insert(4);
+tree.insert(20);
+tree.insert(1);
+tree.insert(6);
+tree.insert(15);
+tree.insert(170);
+
+//        9
+//      /    \
+//    4       20
+//   /  \     /  \
+//  1    6   15  170
+
+console.log(tree.breadthFirstSearchR([tree.root], []));

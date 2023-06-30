69e5e58e73ac89d2cd698a500bb32c90e7671e29
Boney Patel <boneypatel37@yahoo.com>
2023-06-30 17:22:39 -0400

    Dynamic Programming Fibonacci

--- Algorithms/fibonacci.js
+++ Algorithms/fibonacci.js
@@ -1,10 +1,36 @@
 // Write a recursive implementation of the Fibonacci Sequence in JavaScript
+let calculations = 0;
 function fibonacciRecursive(target) {
+    calculations++;
     if (target < 2) {
         return target;
     }
     return fibonacciRecursive(target-1) + fibonacciRecursive(target-2);
 }
 
+console.log(fibonacciRecursive(10));
+console.log("Recursive: We did " + calculations + " calculations.");
 
-console.log(fibonacciRecursive(30));
+
+calculations = 0;
+function fibonacciMaster() {
+    let cache = {};
+    return function fib(n) {
+        calculations++;
+        if (n in cache) {
+            return cache[n];
+        } else {
+            if (n < 2) {
+                return n;
+            } else {
+                cache[n] = fib(n-1) + fib(n-2);
+                return cache[n];
+            }
+
+        }
+    }
+}
+
+const fasterFib = fibonacciMaster();
+console.log(fasterFib(10));
+console.log('Dynamic Programming and Memoization: We did ' + calculations + ' calculations.')
